1fbdec01c58ff3e36d45b2098a53257845276544
ayu243sh-tech <ayu243sh@gmail.com>
2026-08-02 20:56:20 +0530

    Update models3d_data.py

--- Backend/models3d_data.py
+++ Backend/models3d_data.py
@@ -47,7 +47,7 @@ MODELS_3D = {
         "title": "Human Skull",
         "subject": "Biology",
         "class_level": "11",
-        "model_url": "https://khvmrhnkooehyojkguev.supabase.co/storage/v1/object/public/models3d/scene-custom%20(1).glb",
+        "model_url": "https://khvmrhnkooehyojkguev.supabase.co/storage/v1/object/public/models3d/skull_%203d200_100000-custom.glb",
         "hotspots": [],
     },
     "pelvis": {
