21758bbdcf99af102280f1c1e157cda68423bf21
ayu243sh-tech <ayu243sh@gmail.com>
2026-09-24 11:33:47 +0530

    Create resources_approved.py

---
+++ Backend/resources_approved.py
@@ -0,0 +1,8 @@
+# resources_approved.py
+# Manually maintained list of approved Google emails for EduMotion Resources.
+# To grant access: add their email here, commit, push.
+
+APPROVED_EMAILS = {
+    "ayush.example@gmail.com",
+    # add more approved emails here
+}
